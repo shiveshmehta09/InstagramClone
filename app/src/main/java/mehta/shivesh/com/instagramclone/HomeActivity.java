9cd06189bde6000bdef58d919850cf3546253907
Babu <shivesh.mehta@ozoprop.com>
2018-02-24 14:08:37 +0530

    Initial commit

---
+++ app/src/main/java/mehta/shivesh/com/instagramclone/HomeActivity.java
@@ -0,0 +1,13 @@
+package mehta.shivesh.com.instagramclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
